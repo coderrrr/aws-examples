d406007e1c70229eb02cde750d3f9c68034ae129
coderrr <coderrr@amazon.com>
2023-10-09 11:39:38 +0800

    Query EC2 by Tags

--- batch-create-ec2-go/batch-create-ec2.go
+++ batch-create-ec2-go/batch-create-ec2.go
@@ -14,8 +14,8 @@ import (
 
 func main() {
 	// 配置AWS SDK，使用您的AK/SK]
-	AK := "AK12345678901234567890"
-	SK := "12345678901234567890"
+	AK := "AK********"
+	SK := "**********"
 
 	cfg, err := config.LoadDefaultConfig(context.TODO(),
 		config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider(AK, SK, "")),
@@ -30,11 +30,11 @@ func main() {
 
 	// 定义实例配置
 	instanceType := "t3.medium"
-	ebsVolumeSize := 30                // GB
-	securityGroupID := "sg-1234567890" // 替换为您的安全组ID
-	subnetID := "subnet-1234567890"    // 替换为您的子网ID
-	amiID := "ami-079cd5448deeace01"   // 替换为您的AMI ID
-	nameTag := "MyInstance"            // 实例的Name标签
+	ebsVolumeSize := 30              // 30GB
+	securityGroupID := "sg-12345678" // 替换为您的安全组ID
+	subnetID := "subnet-12345678"    // 替换为您的子网ID
+	amiID := "ami-079cd5448deeace01" // 替换为您的AMI ID
+	nameTag := "MyInstance"          // 实例的Name标签
 
 	// 创建EC2实例
 	runInput := &ec2.RunInstancesInput{
@@ -110,6 +110,9 @@ func main() {
 
 		fmt.Printf("为实例 %s 绑定了Elastic IP %s\n", *instance.InstanceId, *allocateOutput.PublicIp)
 	}
+
+	// 查询符合条件的EC2
+	queryEC2s(svc)
 }
 
 // 等待EC2实例状态变为"running"
@@ -139,5 +142,60 @@ func waitForInstancesRunning(ctx context.Context, svc *ec2.Client, instanceIds [
 
 		fmt.Println("等待中...")
 		time.Sleep(10 * time.Second)
+	}
+}
+
+// 根据 Tag 作为过滤条件查询EC2列表，获取EC2的Tag和Public IP
+func queryEC2s(svc *ec2.Client) {
+	// 定义要查询的Tag Name和Value
+	tagName1 := "Name"        // 替换为您的第一个Tag Name
+	tagValue1 := "MyInstance" // 替换为您的第一个Tag Value
+	tagName2 := "Name"        // 替换为您的第二个Tag Name
+	tagValue2 := "MyInstance" // 替换为您的第二个Tag Value
+
+	// 构建Tag筛选条件
+	tagFilter1 := types.Filter{
+		Name:   aws.String("tag:" + tagName1),
+		Values: []string{tagValue1},
+	}
+
+	tagFilter2 := types.Filter{
+		Name:   aws.String("tag:" + tagName2),
+		Values: []string{tagValue2},
+	}
+
+	// 构建EC2实例筛选条件
+	instanceFilters := []types.Filter{tagFilter1, tagFilter2}
+
+	// 查询符合条件的EC2实例
+	describeInstancesOutput, err := svc.DescribeInstances(context.TODO(), &ec2.DescribeInstancesInput{
+		Filters: instanceFilters,
+	})
+
+	if err != nil {
+		fmt.Println("无法查询符合条件的EC2实例:", err)
+		return
+	}
+
+	// 打印符合条件的EC2实例的Tag信息
+	fmt.Println("符合条件的EC2实例列表:")
+	for _, reservation := range describeInstancesOutput.Reservations {
+		for _, instance := range reservation.Instances {
+
+			// 只处理running的实例
+			if *&instance.State.Name != "running" {
+				continue
+			}
+
+			fmt.Printf("Instance ID: %s\n", *instance.InstanceId)
+			fmt.Printf("Public IP: %s\n", *instance.PublicIpAddress)
+
+			// 打印Instance的Tag信息
+			for _, tag := range instance.Tags {
+				fmt.Printf("Tag Name: %s, Tag Value: %s\n", *tag.Key, *tag.Value)
+			}
+
+			fmt.Println()
+		}
 	}
 }
